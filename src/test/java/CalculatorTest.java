7e1f2a05c1a7f030ca02a08e0849a649da1957e9
Chuck Schwing <146346009+ccSchwing@users.noreply.github.com>
2024-02-29 15:06:19 +0000

    AssertTrue

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,45 +1,54 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculatorTest {
 
-    Calculator calculator = new Calculator();
+  Calculator calculator = new Calculator();
 
-    @Test
-    public void testAdd() {
-        int expected = 3;
+  @Test
+  public void testAdd() {
+    int expected = 3;
 
-        int actual = calculator.add(1, 2);
+    int actual = calculator.add(1, 2);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testSubtract() {
-        int expected = 3;
+  @Test
+  public void testSubtract() {
+    int expected = 3;
 
-        int actual = calculator.subtract(5, 2);
+    int actual = calculator.subtract(5, 2);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testMultiply() {
-        int expected = 6;
+  @Test
+  public void testMultiply() {
+    int expected = 6;
 
-        int actual = calculator.multiply(2, 3);
+    int actual = calculator.multiply(2, 3);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testDivide() {
-        int expected = 2;
+  @Test
+  public void testDivide() {
+    int expected = 2;
 
-        int actual = calculator.divide(6, 3);
+    int actual = calculator.divide(6, 3);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void TestIsEven() {
+    boolean expected = true;
+    boolean actual = calculator.isEven(0);
+
+    assertTrue(actual);
+  }
 
 }
